7d8d3de2fc819152e62fbe3b68de3a8bb60e87a6
Bethany Oestrich <bethany@oestrich.org>
2018-04-01 02:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function logWhisper (string)
 function sayHiToGrandma (string)
 {
   if (string === "I love you, Grandma.")
-  return "I love you, too"
+  return "I love you, too."
   else if (string.toUpperCase === string)
-  return "I can't hear you!"
-  else
   return "YES INDEED!"
+  else if (string.toLowerCase === string)
+  return "I can't hear you!"
 }
