bbd952879d0ca8c10b7832bbaa1f28971c31754b
Bethany Oestrich <bethany@oestrich.org>
2018-04-01 02:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function sayHiToGrandma (string)
 {
   if (string === "I love you, Grandma.")
   return "I love you, too"
+  else if (string.toUpperCase === string)
+  return "YES INDEED!"
   else if (string.toLowerCase === string)
   return "I can't hear you!"
-  else (string.toUpperCase === string)
-  return "YES INDEED!"
 }
